0895b9982e6b7f1c62ea9ff480af423c72153064
rravithejareddy <rravithejareddy@gmail.com>
2020-09-17 15:25:48 +0530

    changes for creatign excel for failedusers.

--- readExcel/readExcel.js
+++ readExcel/readExcel.js
@@ -4,19 +4,23 @@ const dynamodb = require('./dynamodb');
 const XLSX = require('xlsx');
 
 module.exports.readExcel = (event, context, callback) => {
+   var failedRows = [];
 
-    var failedRows = [];
     var wb = XLSX.readFile("C:\\Users\\ramireddy\\Documents\\users.xlsx", {
       type: 'binary',
     });
+
+  // console.log(sheetNames);
     var sheetNames = wb.SheetNames;
-    console.log(sheetNames);
+
     var XL_row_object = XLSX.utils.sheet_to_row_object_array(wb.Sheets[wb.SheetNames[0]]);
 
   //  console.log(XL_row_object);
-    XL_row_object.forEach(element => {
+  XL_row_object.forEach(element => {
       var emailReg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
       var phonenoReg = /^\d{4}$/;
+
+   // console.log(element.data)
       if(emailReg.test(element.email) && phonenoReg.test(element.phoneNo)){
       const params = {
         TableName: process.env.DYNAMODB_TABLE,
@@ -27,12 +31,12 @@ module.exports.readExcel = (event, context, callback) => {
           phoneno: element.phoneNo,
           email: element.email,
           city:element.city
-          
-        },
+       },
       };
-   //   console.log(element.data)
+
+  
     
-      // write the todo to the database
+  // write the todo to the database
      dynamodb.put(params, (error) => {
         // handle potential errors
         if (error) {
@@ -56,8 +60,28 @@ module.exports.readExcel = (event, context, callback) => {
     else{
       failedRows.push(element);
     }
-    
-  });
-  console.log("failed rows-"+ JSON.stringify(failedRows));
-  };
+    });
+
+  if(failedRows.length > 0){
+    var data = [];
+    failedRows.forEach(tree => {
+      data.push({id: tree.id, name: tree.name, phoneNo : tree.phoneNo, email : tree.email, city: tree.city });
+    });
+
+      var ws =    XLSX.utils.json_to_sheet(data, {header :['id', 'name', 'phoneNo', 'email', 'city']});
+      console.log(ws);
+      var wb =  XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'users');
+      XLSX.writeFile(wb, "failedUser.xlsx");
+
+       }
+
+  function recurse_bookmarks(data, tree){
+    data.push({id: tree.id, name: tree.name, phoneNo : tree.phoneNo, email : tree.email, city: tree.city })
+  }
+
+  console.log("failed rows-"+ JSON.stringify(failedRows.length));
+};
+
+
 
